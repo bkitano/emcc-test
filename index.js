56fba5d987e84a631be02e4dc87d1287701b75b5
bkitano <briankitano@gmail.com>
2023-06-23 17:29:30 -0400

    adding models

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile("./square.html", {
+  res.sendFile("./index.html", {
     root: __dirname,
     headers: { "Content-Type": "text/html" },
+  });
+});
+
+app.get("/models", (req, res) => {
+  const model = req.query.model;
+  res.sendFile(`./models/ggml-model-whisper-${model}.en.bin`, {
+    root: __dirname,
+    headers: { "Content-Type": "application/octet-stream" },
   });
 });
 
